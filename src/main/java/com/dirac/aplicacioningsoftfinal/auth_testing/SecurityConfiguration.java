aba0411e7eb41740027127978242325e075ec40f
Jean Paul Delgado <jean.delgado@uao.edu.co>
2024-09-23 22:53:00 -0500

    feat(auth): download and testing authentication implemented

--- src/main/java/com/dirac/aplicacioningsoftfinal/auth_testing/SecurityConfiguration.java
+++ src/main/java/com/dirac/aplicacioningsoftfinal/auth_testing/SecurityConfiguration.java
@@ -4,7 +4,6 @@ package com.dirac.aplicacioningsoftfinal.auth_testing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
