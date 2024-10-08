69d002b6a3196f34a785abde9edcfb657bec29fe
Etienne Samboni <etienne.samboni@uao.edu.co>
2024-10-08 13:34:16 -0500

    Implement a proxy in client side for fetching pdf files

--- Client/doctic/src/app/components/PdfViewer.jsx
+++ Client/doctic/src/app/components/PdfViewer.jsx
@@ -4,21 +4,17 @@ import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
+export const PdfViewer = ({ url }) => {
+    const defaultLayoutPluginInstance = defaultLayoutPlugin();
 
-// TODO: Implement the url into the fileUrl when the pdf delivery server or other stuff is hosting the files.
-export const PdfViewer = ({url}) => {
-  const defaultLayoutPluginInstance = defaultLayoutPlugin();
-  return (
-    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.js">
-        <div style={{ height: '750px' }}>
-            <Viewer
-                fileUrl={`http://localhost:8045/pdf/compressed.tracemonkey-pldi-09.pdf`}
-                plugins={[
-                    defaultLayoutPluginInstance, // TODO: Research how to modify the default layout PDF Viewer
-                ]}
-            />
-        </div>
-    </Worker>
-);
-}
-
+    return (
+        <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.js">
+            <div style={{ height: '750px' }}>
+                <Viewer
+                    fileUrl={`/api/proxy?urlArchivo=${encodeURIComponent(url)}`} // Sin comillas y URL codificada
+                    plugins={[defaultLayoutPluginInstance]}
+                />
+            </div>
+        </Worker>
+    );
+};
