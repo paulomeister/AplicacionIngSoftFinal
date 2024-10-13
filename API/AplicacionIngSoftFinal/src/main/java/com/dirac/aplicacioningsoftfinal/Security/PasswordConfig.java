a033f41784c74d4f4275c8380ab88f00069d6e0c
Jean Paul Delgado <jean.delgado@uao.edu.co>
2024-10-13 18:35:05 -0500

    I forgot to define the passwordEncoder method as a Spring Bean so it can inject it wherever it's needed.

--- API/AplicacionIngSoftFinal/src/main/java/com/dirac/aplicacioningsoftfinal/Security/PasswordConfig.java
+++ API/AplicacionIngSoftFinal/src/main/java/com/dirac/aplicacioningsoftfinal/Security/PasswordConfig.java
@@ -1,5 +1,6 @@
 package com.dirac.aplicacioningsoftfinal.Security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -7,6 +8,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class PasswordConfig {
 
+    @Bean
     public PasswordEncoder passwordEncoder() {
 
         return new BCryptPasswordEncoder(10);
