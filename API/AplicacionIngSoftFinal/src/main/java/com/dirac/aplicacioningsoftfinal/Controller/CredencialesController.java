4945b1bcf848b3f7318f9f54a1e4f79a4689c4a5
Jean Paul Delgado <jean.delgado@uao.edu.co>
2024-10-15 18:03:54 -0500

    Sign up successfully working!

---
+++ API/AplicacionIngSoftFinal/src/main/java/com/dirac/aplicacioningsoftfinal/Controller/CredencialesController.java
@@ -0,0 +1,42 @@
+package com.dirac.aplicacioningsoftfinal.Controller;
+
+import com.dirac.aplicacioningsoftfinal.DTO.NuevosCredencialesDTO;
+import com.dirac.aplicacioningsoftfinal.Exception.UserAlreadyExistsException;
+import com.dirac.aplicacioningsoftfinal.Service.ICredencialesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static java.lang.String.*;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/")
+public class CredencialesController {
+
+    private final ICredencialesService credencialesService;
+
+    @Autowired
+    public CredencialesController(ICredencialesService credencialesService) {
+        this.credencialesService = credencialesService;
+    }
+
+    @PostMapping("registrarse")
+    public ResponseEntity<?> crearNuevosCredenciales(@RequestBody NuevosCredencialesDTO credencialesDTO) {
+
+        try {
+
+            credencialesService.crearNuevasCredenciales(credencialesDTO);
+            return ResponseEntity.ok().body(format("El usuario \"%s\" fue agregado con éxito a la base de datos", credencialesDTO.getUsername()));
+
+        }
+        catch(UserAlreadyExistsException e) {
+
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+
+        }
+
+    }
+
+}
