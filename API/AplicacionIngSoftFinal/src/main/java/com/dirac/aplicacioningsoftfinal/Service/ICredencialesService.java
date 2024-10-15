98b87794aa656428a007617c678102580a1b0781
Jean Paul Delgado <jean.delgado@uao.edu.co>
2024-10-15 17:00:10 -0500

    Method added to the contract (interface)

--- API/AplicacionIngSoftFinal/src/main/java/com/dirac/aplicacioningsoftfinal/Service/ICredencialesService.java
+++ API/AplicacionIngSoftFinal/src/main/java/com/dirac/aplicacioningsoftfinal/Service/ICredencialesService.java
@@ -1,9 +1,11 @@
 package com.dirac.aplicacioningsoftfinal.Service;
 
+import com.dirac.aplicacioningsoftfinal.DTO.NuevosCredencialesDTO;
 import com.dirac.aplicacioningsoftfinal.Security.UsuarioAplicacion;
 
 public interface ICredencialesService {
 
     UsuarioAplicacion mapCredentialsFromDatabase(String username);
+    void crearNuevasCredenciales(NuevosCredencialesDTO usuarioEntrante);
 
 }
