2ec88be11acbb3ba57a00d170d7a939c59ec5c0c
Willireyv <williamreyesvalencia1@gmail.com>
2024-10-16 23:06:03 -0500

    a

--- Client/doctic/src/app/document/new/page.js
+++ Client/doctic/src/app/document/new/page.js
@@ -1,6 +1,8 @@
+import Form from './components/Form';
 export default function newdocument() {
     return (
       <div>
+        <Form/>
         <h1>This is the homepage</h1>
       </div>
     );
