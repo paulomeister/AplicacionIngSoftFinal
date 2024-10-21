cde48ed374de44a096908aebdcb4fa8bb9267f69
Willireyv <williamreyesvalencia1@gmail.com>
2024-10-20 22:22:36 -0500

    a

--- Client/doctic/src/app/components/NavBar.jsx
+++ Client/doctic/src/app/components/NavBar.jsx
@@ -1,5 +1,4 @@
-
-"use client"; 
+"use client";
 
 import Link from 'next/link';
 import Image from 'next/image';
@@ -7,6 +6,11 @@ import { useState } from 'react';
 
 const Navbar = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
     <nav style={{ backgroundColor: '#001E58', padding: '10px 20px', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
@@ -16,7 +20,6 @@ const Navbar = () => {
         <h1 style={{ color: 'white', marginLeft: '10px' }}>DocTIC</h1>
       </div>
 
-  
       <div style={{ display: 'flex', gap: '20px' }}>
         <Link href="/" style={{ color: 'white', textDecoration: 'none' }}>
           Inicio
@@ -26,20 +29,42 @@ const Navbar = () => {
         </Link>
       </div>
 
-      
+      <div style={{ position: 'relative' }}>
+        <Image 
+          src={isLoggedIn ? "/user.png" : "/guest.svg"} 
+          alt={isLoggedIn ? "User" : "Guest"} 
+          width={40} 
+          height={40} 
+          onClick={toggleMenu} 
+          style={{ cursor: 'pointer' }} 
+        />
 
-
-      <div>
-      <Link href="/crear-cuenta" style={{ color: 'white', textDecoration: 'none' }}>
-          Crear Cuenta
-        </Link>
-        <Link href="/iniciar-sesion" style={{ color: 'white', textDecoration: 'none' }}>
-          Iniciar Sesión
-        </Link>
-        {isLoggedIn ? (
-          <Image src="/user.png" alt="User" width={40} height={40} /> 
-        ) : (
-          <Image src="/guest.svg" alt="Guest" width={40} height={40} /> 
+        {menuOpen && (
+          <div style={{
+            position: 'absolute',
+            top: '50px',
+            right: '0',
+            backgroundColor: '#fff',
+            boxShadow: '0 8px 16px rgba(0, 0, 0, 0.2)',
+            padding: '10px',
+            borderRadius: '4px'
+          }}>
+            {!isLoggedIn && (
+              <>
+                <Link href="/crear-cuenta" style={{ display: 'block', marginBottom: '10px', color: '#001E58', textDecoration: 'none' }}>
+                  Crear Cuenta
+                </Link>
+                <Link href="/iniciar-sesion" style={{ display: 'block', color: '#001E58', textDecoration: 'none' }}>
+                  Iniciar Sesión
+                </Link>
+              </>
+            )}
+            {isLoggedIn && (
+              <Link href="/perfil" style={{ display: 'block', color: '#001E58', textDecoration: 'none' }}>
+                Ver Perfil
+              </Link>
+            )}
+          </div>
         )}
       </div>
     </nav>
