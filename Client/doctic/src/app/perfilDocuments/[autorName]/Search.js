d7b945a06d39414ac3dfe9cf503631b7aa5f4532
Stevenjr2810 <gerson_steven.lopez@uao.edu.co>
2024-10-21 10:21:13 -0500

    style(ui): customize search bar styling

--- Client/doctic/src/app/perfilDocuments/[autorName]/Search.js
+++ Client/doctic/src/app/perfilDocuments/[autorName]/Search.js
@@ -22,6 +22,11 @@ export default function Search({ setSearchTitle, setFilterCategory, setFilterIdi
     setFilters(filters.filter((filter) => filter.id !== id)); // Elimina el filtro por su id
   };
 
+  // Función para eliminar todos los filtros
+  const handleRemoveAllFilters = () => {
+    setFilters([]);
+  }
+
   // Función para actualizar el tipo de filtro
   const handleFilterTypeChange = (id, newType) => {
     setFilters(filters.map((filter) =>
@@ -61,132 +66,147 @@ export default function Search({ setSearchTitle, setFilterCategory, setFilterIdi
   };
 
   return (
-   <div className="container mx-auto p-6 flex-grow bg-[#DFE4F8] rounded relative mt-8 shadow-lg">
-          
-      {/* Barra de Búsqueda */}
-  <div className="relative flex items-center">
-    {/* Ícono FaSearch posicionado dentro del input */}
-    <div className="absolute left-4 inset-y-0 flex items-center pointer-events-none">
-      <FaSearch className="text-gray-400" />
-    </div>
-    
-    <input
-      type="text"
-      value={searchQuery}
-      onChange={(e) => setSearchQuery(e.target.value)} // Actualiza el estado al escribir en la barra
-      placeholder="Buscar algo..."
-      className="w-full p-6 pl-12 rounded-full bg-white focus:outline-none focus:ring-4 focus:ring-blue-600 text-lg"
-    />
-  </div>
-
-      {/* Botón estilo "pestaña" para abrir/cerrar filtros */}
+    <div className="container mx-auto p-6 flex-grow bg-[#f3f0ff] rounded relative mt-8 ">
+    {/* Barra de Búsqueda */}
+    <div className="relative flex items-center bg-[#f3f0ff] ">
+      {/* Ícono FaSearch posicionado dentro del input como botón */}
       <button
-          onClick={toggleFilters}
-          className="absolute left-1/2 transform -translate-x-1/2 bottom-[-20px] bg-[#DFE4F8] text-gray-700 rounded-b-full px-8 py-2 focus:outline-none"
+        onClick={handleSearch} // Llama a la función de búsqueda
+        className="absolute right-4 inset-y-0 flex items-center text-gray-400 hover:text-blue-600 focus:outline-none"
       >
-          {showFilters ? "Cerrar filtro" : "Abrir filtro"}
+        <FaSearch className="text-2xl"/>
       </button>
-
-      {/* Sección de filtros */}
-      {showFilters && (
-          <div className="mt-12 p-6 bg-[#DFE4F8] rounded-b-lg shadow-lg">
-          
-          {/* Lista de filtros dinámicos */}
-          {filters.map((filter) => (
-              <div key={filter.id} className="flex gap-4 items-center mb-4">
-              
-              {/* Selector de tipo de filtro */}
-              <select
-                  className="p-4 bg-white rounded-md w-1/4"
-                  value={filter.type}
-                  onChange={(e) => handleFilterTypeChange(filter.id, e.target.value)}
-              >
-                  <option value="categoria">Filtrar por Categoría</option>
-                  <option value="idioma">Filtrar por Idioma</option>
-                  <option value="fecha">Filtrar por Fechas</option>
-              </select>
-
-              {/* Renderiza el input basado en el tipo de filtro seleccionado */}
-              {filter.type === "categoria" ? (
-                  <input
-                  type="text"
-                  className="p-4 bg-white rounded-md flex-grow"
-                  placeholder="Escribir Categoría"
-                  value={filter.value}
-                  onChange={(e) => handleFilterValueChange(filter.id, e.target.value)}
-                  />
-              ) : filter.type === "idioma" ? (
-                <input
+      
+      <input
+        type="text"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)} // Actualiza el estado al escribir en la barra
+        placeholder="Buscar algo..."
+        className="w-full p-6 pl-12 rounded-full bg-white focus:outline-none focus:ring-4 focus:ring-blue-600 text-lg "
+      />
+    </div>   
+    <hr className="my-4 border-t-2 border-gray-400" />
+    {/* Botón estilo "pestaña" para abrir/cerrar filtros */}
+    <button
+      onClick={toggleFilters}
+      className="absolute left-1/2 transform -translate-x-1/2 bottom-[-20px] bg-[#f3f0ff] text-gray-700 rounded-b-full px-8 py-2 focus:outline-none"
+    >
+      {showFilters ? "Ocultar filtros" : "Mostrar filtros"}
+    </button>
+  
+    {/* Sección de filtros */}
+    {showFilters && (
+      
+      <div className="mt-12 p-6 bg-[#f3f0ff] rounded-b-lg ">
+        
+        {/* Lista de filtros dinámicos */}
+        {filters.map((filter) => (
+          <div key={filter.id} className="flex gap-4 items-center mb-4">
+            
+            {/* Selector de tipo de filtro */}
+            <select
+              className="p-4 bg-white rounded-md w-1/4"
+              value={filter.type}
+              onChange={(e) => handleFilterTypeChange(filter.id, e.target.value)}
+            >
+              <option value="categoria">Filtrar por Categoría</option>
+              <option value="idioma">Filtrar por Idioma</option>
+              <option value="fecha">Filtrar por Fechas</option>
+            </select>
+  
+            {/* Renderiza el input basado en el tipo de filtro seleccionado */}
+            {filter.type === "categoria" ? (
+              <input
+                type="text"
+                className="p-4 bg-white rounded-md flex-grow"
+                placeholder="Escribir Categoría"
+                value={filter.value}
+                onChange={(e) => handleFilterValueChange(filter.id, e.target.value)}
+              />
+            ) : filter.type === "idioma" ? (
+              <input
                 type="text"
                 className="p-4 bg-white rounded-md flex-grow"
                 placeholder="Escribir Idioma"
                 value={selectedIdioma}  // Usamos el estado `selectedIdioma`
                 onChange={(e) => setSelectedIdioma(e.target.value)}  // Actualiza el idioma
-                /> 
-              
-              ) : filter.type === "fecha" ? (
-                  <div className="flex flex-col gap-2">
-                  <label>
-                      Desde (Año):
-                      <input
-                      type="number"
-                      className="p-2 bg-white rounded-md"
-                      value={dateRange.from || ""}
-                      onChange={(e) => handleDateChange("from", e.target.value)}  // Actualiza el año de inicio
-                      />
-                  </label>
-                  <label>
-                      Hasta (Año):
-                      <input
-                      type="number"
-                      className="p-2 bg-white rounded-md"
-                      value={dateRange.to || ""}
-                      onChange={(e) => handleDateChange("to", e.target.value)}  // Actualiza el año de finalización
-                      />
-                  </label>
-                  </div>
-              ) : null}
-
-              {/* Botón para eliminar el filtro */}
-              <button 
-                  onClick={() => handleRemoveFilter(filter.id)} // Elimina el filtro
-                  className="p-2 text-red-500">
-                  <svg
-                  className="w-8 h-8"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                  >
-                  <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M6 18L18 6M6 6l12 12"
-                  ></path>
-                  </svg>
-              </button>
+              /> 
+            ) : filter.type === "fecha" ? (
+              <div className="flex flex-col gap-2">
+                <label>
+                  Desde (Año):
+                  <input
+                    type="number"
+                    className="p-2 bg-white rounded-md"
+                    value={dateRange.from || ""}
+                    onChange={(e) => handleDateChange("from", e.target.value)}  // Actualiza el año de inicio
+                  />
+                </label>
+                <label>
+                  Hasta (Año):
+                  <input
+                    type="number"
+                    className="p-2 bg-white rounded-md"
+                    value={dateRange.to || ""}
+                    onChange={(e) => handleDateChange("to", e.target.value)}  // Actualiza el año de finalización
+                  />
+                </label>
               </div>
-          ))}
-
-          {/* Botón para agregar un nuevo filtro */}
-          <button 
-              onClick={handleAddFilter} 
-              className="mt-4 text-blue-600 text-lg">
-              + Agregar nuevo filtro
-          </button>
+            ) : null}
+  
+            {/* Botón para eliminar el filtro */}
+            <button 
+              onClick={() => handleRemoveFilter(filter.id)} // Elimina el filtro
+              className="p-2 text-red-500 ">
+              <svg
+                className="w-8 h-8"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                ></path>
+              </svg>
+            </button>
           </div>
-      )}
-
-      {/* Botón de Búsqueda */}
-      <div className="mt-6">
-          <button 
-          onClick={handleSearch}
-          className="w-full p-4 bg-blue-500 text-white rounded-lg text-xl focus:outline-none focus:ring-4 focus:ring-blue-500">
-          Buscar
+        ))}
+  
+        {/* Botón para agregar un nuevo filtro */}
+        <button 
+          onClick={handleAddFilter} 
+          className="mt-4 text-blue-600 text-lg ">
+          + Agregar nuevo filtro
+        </button>
+  
+        {/* Botón de Búsqueda y Eliminar Filtros */}
+        <div className="flex justify-end gap-4 mt-6">
+          {/* Botón de Búsqueda */}
+          <button
+            className="px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-300"
+            onClick={handleSearch}
+          >
+            Buscar
           </button>
+
+          {/* Botón de Eliminar todos los filtros */}
+          <button
+            className="px-6 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 focus:outline-none focus:ring-4 focus:ring-red-300"
+            onClick={handleRemoveAllFilters}
+          >
+            Eliminar Filtros
+          </button>
+        </div>
+  
+        
+        
       </div>
-    </div>
+    )}
+  </div>
   );
 }
 
