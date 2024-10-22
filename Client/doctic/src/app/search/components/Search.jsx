4e1de0bc72b2be01a442775608d33d32aa079b79
Gullit0026 <juliansalamancaa@gmail.com>
2024-10-22 10:11:32 -0500

    cambios en Search.jsx

--- Client/doctic/src/app/search/components/Search.jsx
+++ Client/doctic/src/app/search/components/Search.jsx
@@ -1,7 +1,6 @@
 'use client';
 import React, { useState } from "react";
 import ResultsList from "./ResultsList";
-import SortMenu from "./SortMenu";
 import Filter from "./Filter";
 import "./Search.css";
 
