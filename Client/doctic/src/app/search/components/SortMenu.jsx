9efef425163d4ba729a6c94dd32c0cc8b9d4bd63
Gullit0026 <juliansalamancaa@gmail.com>
2024-10-22 10:29:54 -0500

    nuevos camios

--- Client/doctic/src/app/search/components/SortMenu.jsx
+++ Client/doctic/src/app/search/components/SortMenu.jsx
@@ -1,21 +1,38 @@
 'use client';
 import React from 'react';
+import { useState } from 'react';
 import './SortMenu.css';
 
 const SortMenu = ({ onSortChange }) => {
+  const [selectedOption, setSelectedOption] = useState('');
+
   const handleSort = (sortOption) => {
+    setSelectedOption(sortOption);
     onSortChange(sortOption); 
   };
 
   return (
     <div className="sort-menu">
+      <strong>Menú de ordenamiento</strong>
       <ul className="sort-options">
-        <li onClick={() => handleSort("alfabetico")}>Alfabetico [A - Z]</li>
-        <li onClick={() => handleSort("alfabetico-inverso")}>Alfabetico [Z - A]</li>
-        <li onClick={() => handleSort("fecha-asc")}>Fecha (ascendente)</li>
-        <li onClick={() => handleSort("fecha-desc")}>Fecha (descendente)</li>
-        <li onClick={() => handleSort("valoracion-asc")}>Valoración (ascendente)</li>
-        <li onClick={() => handleSort("valoracion-desc")}>Valoración (descendente)</li>
+        <li className={selectedOption === 'alfabetico' ? 'selected' : ''} onClick={() => handleSort("alfabetico")}>
+          Alfabetico [A - Z]
+        </li>
+        <li className={selectedOption === 'alfabetico-inverso' ? 'selected' : ''} onClick={() => handleSort("alfabetico-inverso")}>
+          Alfabetico [Z - A]
+        </li>
+        <li className={selectedOption === 'fecha-asc' ? 'selected' : ''} onClick={() => handleSort("fecha-asc")}>
+          Fecha (ascendente)
+        </li>
+        <li className={selectedOption === 'fecha-desc' ? 'selected' : ''} onClick={() => handleSort("fecha-desc")}>
+          Fecha (descendente)
+        </li>
+        <li className={selectedOption === 'valoracion-asc' ? 'selected' : ''} onClick={() => handleSort("valoracion-asc")}>
+          Valoración (ascendente)
+        </li>
+        <li className={selectedOption === 'valoracion-desc' ? 'selected' : ''} onClick={() => handleSort("valoracion-desc")}>
+          Valoración (descendente)
+        </li>
       </ul>
     </div>
   );
