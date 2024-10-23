72ac56aa2157109aa8a994a4ed609a9eda51d1a1
sergio <sergioandresherreravallejo@gmail.com>
2024-10-23 01:17:25 -0500

    Funcionalidad de Eliminar los documentos de un autor (por el mismo autor) TERMINADA

--- Client/doctic/src/app/perfilDocuments/[autorName]/List.js
+++ Client/doctic/src/app/perfilDocuments/[autorName]/List.js
@@ -2,35 +2,45 @@
 
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { FaCalendarAlt, FaEye, FaEdit, FaTrash } from "react-icons/fa";
-import Link from "next/link";
 import { SpinerComp } from "app/app/document/[id]/components/SpinnerComp";
+import { DocumentListItem } from "./DocumentListItem";
 
-export default function List({ autorName, searchTitle, filterCategory, filterIdioma, filterDates, setAuthorNameFromList }) {
+export default function List({
+  autorName,
+  searchTitle,
+  filterCategory,
+  filterIdioma,
+  filterDates,
+  setAuthorNameFromList,
+}) {
   const [documents, setDocuments] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  // FUNCTION FOR MODAL:
+
   const fetchDocuments = async () => {
     setLoading(true);
     try {
-      const response = await axios.post("http://localhost:8080/api/Documentos/onSearch/filter/", {
-        titulo: searchTitle,  // Aquí pasamos el título que viene del componente Search
-        tieneFiltros: true,
-        categorias: filterCategory,
-        autores: [autorName],
-        idioma: filterIdioma,
-        desde: filterDates.from ? parseInt(filterDates.from) : "",  // Convertir las fechas a enteros
-        hasta: filterDates.to ? parseInt(filterDates.to) : "",
-      });
+      const response = await axios.post(
+        "http://localhost:8080/api/Documentos/onSearch/filter/",
+        {
+          titulo: searchTitle, // Aquí pasamos el título que viene del componente Search
+          tieneFiltros: true,
+          categorias: filterCategory,
+          autores: [autorName],
+          idioma: filterIdioma,
+          desde: filterDates.from ? parseInt(filterDates.from) : "", // Convertir las fechas a enteros
+          hasta: filterDates.to ? parseInt(filterDates.to) : "",
+        }
+      );
       setDocuments(response.data);
-      
+
       // Si obtenemos documentos, actualizamos el nombre del autor
       if (response.data.length > 0) {
-        const firstAuthorName = response.data[0].autores[0]?.nombre;  // Obtenemos el primer autor
-        setAuthorNameFromList(firstAuthorName);  // Actualizamos el autor en PerfilDocuments
+        const firstAuthorName = response.data[0].autores[0]?.nombre; // Obtenemos el primer autor
+        setAuthorNameFromList(firstAuthorName); // Actualizamos el autor en PerfilDocuments
       }
-
     } catch (err) {
       setError(err.message);
     } finally {
@@ -42,7 +52,7 @@ export default function List({ autorName, searchTitle, filterCategory, filterIdi
     if (autorName) {
       fetchDocuments();
     }
-  }, [autorName, searchTitle, filterCategory, filterIdioma, filterDates]);  // Actualiza la lista cuando cambie cualquier filtro
+  }, [autorName, searchTitle, filterCategory, filterIdioma, filterDates]); // Actualiza la lista cuando cambie cualquier filtro
 
   const handleEdit = (id) => {
     // Funcionalidad futura para editar el documento
@@ -50,68 +60,42 @@ export default function List({ autorName, searchTitle, filterCategory, filterIdi
   };
 
   const handleDelete = (id) => {
-    // Funcionalidad futura para eliminar el documento
-    console.log(`Eliminar documento con ID: ${id}`);
+
+    async function fetchToDelete() {
+
+      const data=  await fetch(`http://localhost:8080/api/Documentos/delete/${id}`,{method:"DELETE"})
+      const response = await data.json();
+
+      if (response.status === 200) {
+        fetchDocuments();
+      } else {
+        return;
+      }
+
+    }
+
+
+    fetchToDelete();
   };
 
   if (loading) return <SpinerComp />;
-  if (error) return <p className="text-center text-red-500 mt-8 text-lg">Error: {error}</p>;
+  if (error)
+    return (
+      <p className="text-center text-red-500 mt-8 text-lg">Error: {error}</p>
+    );
 
   return (
     <div className="container  p-6 flex-grow">
-      
       <ul className="space-y-8">
         {documents.map((doc) => (
-          <li key={doc._id} className="p-6 bg-white rounded shadow-md border">
-            <h2 className="text-2xl font-semibold mb-2">{doc.titulo}</h2>
-            <p className="text-lg text-gray-700 mb-1">
-              <span className="font-semibold">Subido por:</span> {doc.autores[0]?.nombre}
-            </p>
-            <p className="text-base text-gray-600 mb-2 font-semibold">
-              {doc.categoria.map((cat) => cat.nombre).join(", ")}
-            </p>
-
-            <div className="flex justify-between items-center mt-4">
-              <div className="flex gap-8">
-                <div className="flex items-center gap-2 text-gray-600">
-                  <FaCalendarAlt />
-                  <span>
-                    {new Date(doc.fechaSubida).toLocaleDateString("es-ES", {
-                      day: "2-digit",
-                      month: "long",
-                      year: "numeric",
-                    })}
-                  </span>
-                </div>
-              </div>
-
-              <div className="flex gap-4">
-                <Link href={`/document/${doc._id}`} className="flex items-center gap-2 text-blue-600 hover:underline">
-                  <FaEye /> Ver
-                </Link>
-
-                {/* Botón para Editar */}
-                <button 
-                  onClick={() => handleEdit(doc._id)} 
-                  className="flex items-center gap-2 text-green-500 hover:underline"
-                >
-                  <FaEdit /> Editar
-                </button>
-
-                {/* Botón para Eliminar */}
-                <button 
-                  onClick={() => handleDelete(doc._id)} 
-                  className="flex items-center gap-2 text-red-500 hover:underline"
-                >
-                  <FaTrash /> Eliminar
-                </button>
-              </div>
-            </div>
-          </li>
+          <DocumentListItem
+            key={doc._id}
+            doc={doc}
+            handleDelete={handleDelete}
+            handleEdit={handleEdit}
+          ></DocumentListItem>
         ))}
       </ul>
     </div>
   );
 }
-
-
