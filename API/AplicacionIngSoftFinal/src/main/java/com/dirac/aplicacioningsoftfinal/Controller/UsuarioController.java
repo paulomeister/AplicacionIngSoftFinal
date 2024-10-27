7bd19319b374143a221d8285502e637bc675600c
sergio <sergioandresherreravallejo@gmail.com>
2024-10-26 20:59:52 -0500

    End point para subir la imagen a IMGUR, el ImgurService también fue implementado.

--- API/AplicacionIngSoftFinal/src/main/java/com/dirac/aplicacioningsoftfinal/Controller/UsuarioController.java
+++ API/AplicacionIngSoftFinal/src/main/java/com/dirac/aplicacioningsoftfinal/Controller/UsuarioController.java
@@ -1,15 +1,25 @@
 package com.dirac.aplicacioningsoftfinal.Controller;
 
+import com.dirac.aplicacioningsoftfinal.DTO.Res;
 import com.dirac.aplicacioningsoftfinal.Exception.UsuarioNotFoundException;
 import com.dirac.aplicacioningsoftfinal.Model.UsuarioModel;
+import com.dirac.aplicacioningsoftfinal.Service.DriveService;
 import com.dirac.aplicacioningsoftfinal.Service.IUsuarioService;
+import com.dirac.aplicacioningsoftfinal.Service.ImgurService;
 
+import jakarta.websocket.server.PathParam;
+
+import org.apache.catalina.connector.Response;
+import org.apache.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/Usuarios")
@@ -17,6 +27,8 @@ public class UsuarioController {
 
     @Autowired
     private IUsuarioService usuarioService;
+
+    private ImgurService imgurService = new ImgurService();
 
     @GetMapping("/id/{id}")
     public ResponseEntity<?> getUserById(@PathVariable("id") String id) {
@@ -37,6 +49,16 @@ public class UsuarioController {
     @GetMapping("/getAllUsers")
     ResponseEntity<?> getAllUsers() {
         return ResponseEntity.status(200).body(usuarioService.getAll());
+    }
+
+    @PostMapping("/uploadProfilePic")
+    public ResponseEntity<?> uploadImage(@RequestParam("image") MultipartFile image) {
+        try {
+            String imageUrl = imgurService.uploadImage(image);
+            return ResponseEntity.ok().body(imageUrl);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(e.getMessage());
+        }
     }
 
 }
--- API/AplicacionIngSoftFinal/src/main/java/com/dirac/aplicacioningsoftfinal/Service/DriveService.java
+++ API/AplicacionIngSoftFinal/src/main/java/com/dirac/aplicacioningsoftfinal/Service/DriveService.java
@@ -6,6 +6,7 @@ import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.ByteArrayContent;
 import com.google.api.client.http.FileContent;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
@@ -15,6 +16,8 @@ import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.drive.model.File;
 import com.google.api.services.drive.model.FileList;
+
+import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -23,56 +26,92 @@ import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.List;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public class DriveService {
-  private static final String APPLICATION_NAME = "Google Drive API Java Quickstart";
-  private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-  private static final String TOKENS_DIRECTORY_PATH = "tokens";
-  private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE_FILE);
-  private static final String CREDENTIALS_FILE_PATH = "C:/Users/sergi/Documents/university/fifth semester/ingesoft/AplicacionIngSoftFinal/credentials.json";
 
-  private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
-    InputStream in = DriveService.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
-    if (in == null) {
-      throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+ //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private static final String APPLICATION_NAME = "AplicacionIngSoftFinal";
+    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+    private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE_FILE);
+    private static final String CREDENTIALS_FILE_PATH = "./credentials.json";
+    private final static String folderId = "1mdVe9JNnoWZKE7eN9n-szy4Zk2u8DAuV";
+
+    private Credential getDriveCredentials(NetHttpTransport HTTP_TRANSPORT)
+            throws GeneralSecurityException, IOException {
+
+        InputStream in = DriveService.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        if (in == null) {
+            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+        }
+
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in)); // Secrets
+
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).setCallbackPath("/Callback")
+                .build();
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+
     }
-    GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
-    GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-        HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-        .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-        .setAccessType("offline")
-        .build();
-    LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-    return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
-  }
+    public Drive createDrive() throws GeneralSecurityException, IOException {
 
-  public static void main(String... args) throws IOException, GeneralSecurityException {
-    final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-    Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-        .setApplicationName(APPLICATION_NAME)
-        .build();
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport(); // TRANSPORT
 
-    // Crear una carpeta en Google Drive
-    File folderMetadata = new File();
-    folderMetadata.setName("NuevaCarpeta");
-    folderMetadata.setMimeType("application/vnd.google-apps.folder");
+        Drive drive = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getDriveCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+        return drive;
+    }
 
-    File folder = service.files().create(folderMetadata)
-        .setFields("id")
-        .execute();
-    String folderId = folder.getId();
+    public String uploadToDrive(MultipartFile file) throws GeneralSecurityException, IOException {
 
-    // Subir archivo PDF a la carpeta creada
-    File fileMetadata = new File();
-    fileMetadata.setName("hola.pdf");
-    fileMetadata.setParents(Collections.singletonList(folderId));
+        String res;
 
-    java.io.File filePath = new java.io.File("hola.pdf");
-    FileContent mediaContent = new FileContent("application/pdf", filePath);
+        try {
 
-    File file = service.files().create(fileMetadata, mediaContent)
-        .setFields("id, parents")
-        .execute();
+            Drive drive = createDrive(); // Crea el documento drive
 
-  }
+            com.google.api.services.drive.model.File fileMetaData = new com.google.api.services.drive.model.File();
+            fileMetaData.setName(file.getOriginalFilename());
+            fileMetaData.setParents(Collections.singletonList(folderId));
+
+            // Contenido del header
+            ByteArrayContent mediaContent = new ByteArrayContent("application/pdf", file.getBytes());
+
+            // Sube el archivo a Google Drive
+            com.google.api.services.drive.model.File uploadedFile = drive.files().create(fileMetaData, mediaContent)
+                    .setFields("id").execute();
+
+            // retorna el id de lo que se subió
+            res = uploadedFile.getId();
+        } catch (Exception e) {
+            res = e.getMessage();
+        }
+        return res;
+    }
+
+    public com.google.api.services.drive.model.File getFileById(String fileId)
+            throws GeneralSecurityException, IOException {
+        Drive drive = createDrive();
+        return drive.files().get(fileId).setFields("id, name, mimeType").execute();
+    }
+
+    public byte[] downloadFile(String fileId) throws GeneralSecurityException, IOException {
+
+        Drive drive = createDrive(); // crea el servicio de drive
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        drive.files().get(fileId).executeMediaAndDownloadTo(outputStream);
+        return outputStream.toByteArray(); // Devuelve el contenido del archivo como byte[]
+    }
+  
 }
