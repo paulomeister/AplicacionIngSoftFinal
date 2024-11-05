f40972d2de823607d860eb1296e50874366fe8d5
Gullit0026 <juliansalamancaa@gmail.com>
2024-11-05 03:20:16 -0500

    actualización desde main

--- Client/doctic/src/app/users/[username]/components/DocumentList.jsx
+++ Client/doctic/src/app/users/[username]/components/DocumentList.jsx
@@ -5,7 +5,6 @@ import DocumentItem from "./DocumentItem";
 import { AlertPop } from "../utils/AlertPopup";
 import Link from "next/link";
 import "./DocumentList.css";
-import Link from "next/link";
 
 const DocumentList = ({ autor, onVerTodos, propietario }) => {
 
