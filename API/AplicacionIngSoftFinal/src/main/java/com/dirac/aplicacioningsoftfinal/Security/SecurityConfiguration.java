cb1ff5ed8098bf380469bb1377d49e2f4cb7fab1
Jean Paul Delgado <jean.delgado@uao.edu.co>
2024-11-09 22:01:10 -0500

    Killing CORS at the moment

--- API/AplicacionIngSoftFinal/src/main/java/com/dirac/aplicacioningsoftfinal/Security/SecurityConfiguration.java
+++ API/AplicacionIngSoftFinal/src/main/java/com/dirac/aplicacioningsoftfinal/Security/SecurityConfiguration.java
@@ -15,12 +15,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import javax.crypto.SecretKey;
-
-import java.util.Arrays;
 
 import static com.dirac.aplicacioningsoftfinal.Security.PermisosDeUsuarioPorRol.ADMIN;
 import static com.dirac.aplicacioningsoftfinal.Security.PermisosDeUsuarioPorRol.USUARIO;
@@ -48,6 +44,7 @@ public class SecurityConfiguration {
         
         http
                 .csrf((csrf) -> csrf.disable())
+                .cors((cors) -> cors.disable())
                 .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilter(new CustomUsernameAndPasswordAuthenticationFilter(authenticationManager, jwtConfigurationVariables, secretKey))
                 .addFilterAfter(new JwtTokenVerifier(secretKey, jwtConfigurationVariables), CustomUsernameAndPasswordAuthenticationFilter.class)
@@ -113,21 +110,6 @@ public class SecurityConfiguration {
                 .passwordEncoder(passwordEncoder);
 
         return authenticationManagerBuilder.build();
-    }
-
-    @Bean
-    UrlBasedCorsConfigurationSource corsConfigurationSource() {
-
-        CorsConfiguration configuration = new CorsConfiguration();
-
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-
-        return source;
-
     }
 
 }
