919f6ceba5320107c7f805378e78e5761dba43ed
Stevenjr2810 <gerson_steven.lopez@uao.edu.co>
2024-11-14 01:11:30 -0500

    Corregido algunos errores de diseño y funcionalidad con autenticacion para buscar los documentos publicados por el autor

--- Client/doctic/src/app/home/components/RecentDocuments.js
+++ Client/doctic/src/app/home/components/RecentDocuments.js
@@ -94,10 +94,10 @@ export default function Documentos() {
         <ul className="space-y-8">
           {documents.map((doc) => (
             <li key={doc._id} className="p-6 bg-white rounded shadow-md border">
-              <Link href={`/document/${doc._id}`} className="hover:text-blue-600 hover:underline"><h2 className="text-2xl font-semibold mb-2">{doc.titulo}</h2></Link>
+              <Link href={`/document/${doc._id}`} className="hover:text-blue-600 no-underline hover:underline"><h2 className=" text-2xl font-semibold mb-2">{doc.titulo}</h2></Link>
               <p className="text-lg text-gray-700 mb-1">
                 <span className="font-semibold">Subido por:</span> 
-                <Link href={`/users/${doc.autores?.[0]?.username}`} className="text-blue-600 hover:underline"> {doc.autores?.[0]?.nombre}</Link>
+                <Link href={`/users/${doc.autores?.[0]?.username}`} className="text-blue-600 hover:underline no-underline"> {doc.autores?.[0]?.nombre}</Link>
               </p>
               <p className="text-base text-gray-600 mb-2 font-semibold">
                 {doc.categoria?.map((cat) => cat.nombre).join(", ")}
@@ -118,7 +118,7 @@ export default function Documentos() {
 
                   <div className={`flex items-center gap-2 ${getHighlightClass('rating')}`}>
                     <FaStar />
-                    <span>{doc.datosComputados?.valoracionPromedio == null ? 'Sin calificación' : `${doc.datosComputados?.valoracionPromedio} /5`}</span>
+                    <span>{doc.datosComputados?.valoracionPromedio == null ? 'Sin calificación' : `${Math.floor(doc.datosComputados?.valoracionPromedio * 10) / 10} /5`}</span>
                   </div>
 
                   <div className={`flex items-center gap-2 ${getHighlightClass('downloads')}`}>
@@ -127,7 +127,7 @@ export default function Documentos() {
                   </div>
                 </div>
 
-                <Link href={`/document/${doc._id}`} className="flex items-center gap-2 text-blue-600 hover:underline">
+                <Link href={`/document/${doc._id}`} className="flex items-center gap-2 text-blue-600 hover:underline no-underline">
                   <FaEye /> Ver
                 </Link>
               </div>
--- Client/doctic/src/app/users/perfilDocuments/[autorName]/components/DocumentListItem.js
+++ Client/doctic/src/app/users/perfilDocuments/[autorName]/components/DocumentListItem.js
@@ -1,12 +1,15 @@
+import { useContext } from "react";
+import { AuthContext } from "app/app/context/AuthContext";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import Link from "next/link";
 import { FaEdit, FaEye, FaTrash, FaCalendarAlt } from "react-icons/fa";
 import { useState } from "react";
 
-export function DocumentListItem({ doc, handleEdit, handleDelete, propietario }) {
+export function DocumentListItem({ doc, handleEdit, handleDelete }) {
   const [show, setShow] = useState(false);
-  console.log(doc)
+  const { user } = useContext(AuthContext); // Obtiene el usuario logueado
+
   const handleClose = () => {
     setShow(false);
   };
@@ -14,25 +17,38 @@ export function DocumentListItem({ doc, handleEdit, handleDelete, propietario })
     setShow(true);
   };
 
+  // Verifica si el usuario actual es uno de los autores
+  const isAuthor = doc.autores.some((autor) => autor.usuarioId === user?._id);
+
+  // Filtra la visibilidad: solo muestra privados si el usuario es el autor
+  if (doc.visibilidad === "privado" && !isAuthor) {
+    return null;
+  }
+
   return (
     <li key={doc._id} className="p-6 bg-white rounded shadow-md border">
-      <Link href={`/document/${doc._id}`} className="hover:underline hover:text-blue-600"><h2 className="text-2xl font-semibold mb-2">{doc.titulo}</h2></Link>
+      <Link href={`/document/${doc._id}`} className="hover:underline hover:text-blue-600 no-underline">
+        <h2 className="text-2xl font-semibold mb-2">{doc.titulo}</h2>
+      </Link>
       <p className="text-lg text-gray-700 mb-1">
         <span className="font-semibold">Autores:</span>{" "}
         {doc.autores.map((autor, index) => (
           <span key={autor.username}>
-            <Link href={`/users/${autor.username}`} className="hover:underline hover:text-blue-600">
+            <Link href={`/users/${autor.username}`} className="hover:underline hover:text-blue-600 no-underline">
               {autor.nombre}
             </Link>
-            
             {index < doc.autores.length - 1 && ", "}
           </span>
         ))}
       </p>
 
-      {/* Categorías */}
       <p className="text-base text-gray-600 mb-2 font-semibold">
         {doc.categoria.map((cat) => cat.nombre).join(", ")}
+      </p>
+
+      {/* Etiqueta de Visibilidad */}
+      <p className={`text-sm font-semibold ${doc.visibilidad === "publico" ? "text-green-600" : "text-red-600"}`}>
+        {doc.visibilidad === "publico" ? "Público" : "Privado"}
       </p>
 
       <div className="flex justify-between items-center mt-4">
@@ -50,29 +66,21 @@ export function DocumentListItem({ doc, handleEdit, handleDelete, propietario })
         </div>
 
         <div className="flex gap-4">
-          <Link
-            href={`/document/${doc._id}`}
-            className="flex items-center gap-2 text-blue-600 hover:underline"
-          >
+          <Link href={`/document/${doc._id}`} className="flex items-center gap-2 text-blue-600 hover:underline no-underline">
             <FaEye /> Ver
           </Link>
 
-          <Link
-            href={`/document/edit/${doc._id}`}
-            className="flex items-center gap-2 text-green-500 hover:underline"
-          >
-            <FaEdit /> Editar
-          </Link>
-
-          <>
-            <Button
-              variant="red"
-              className="flex items-center gap-2 bg-white text-red-600 hover:underline"
-              onClick={handleShow}
-            >
-              <FaTrash /> Eliminar
-            </Button>
-          </>
+          {/* Muestra los botones de Editar y Eliminar solo si el usuario es autor */}
+          {isAuthor && (
+            <>
+              <Link href={`/document/edit/${doc._id}`} className="flex items-center gap-2 text-green-500 hover:underline no-underline">
+                <FaEdit /> Editar
+              </Link>
+              <Button variant="red" className="flex items-center text-red-600 hover:text-red-700 p-2" onClick={handleShow}>
+                <FaTrash />
+              </Button>
+            </>
+          )}
         </div>
       </div>
 
@@ -81,16 +89,10 @@ export function DocumentListItem({ doc, handleEdit, handleDelete, propietario })
           <Modal.Title>Eliminar Documento</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          ¿Estás seguro que quieres{" "}
-          <span className="text-red-600 underline">eliminar</span> este
-          documento?
+          ¿Estás seguro que quieres <span className="text-red-600 underline">eliminar</span> este documento?
         </Modal.Body>
         <Modal.Footer>
-          <Button
-            variant="secondary"
-            onClick={handleClose}
-            className="flex items-center gap-2 text-white bg-gray-700 hover:underline"
-          >
+          <Button variant="secondary" onClick={handleClose} className="flex items-center gap-2 text-white bg-gray-700 hover:underline">
             Cerrar
           </Button>
           <Button
@@ -108,3 +110,5 @@ export function DocumentListItem({ doc, handleEdit, handleDelete, propietario })
     </li>
   );
 }
+
+
