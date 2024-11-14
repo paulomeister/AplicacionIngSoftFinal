9e3b59f191f29fc25bb2c59091e8ebca1af6c3df
sergio <sergioandresherreravallejo@gmail.com>
2024-11-14 00:53:19 -0500

    Crud CATEGORIAS LISTO

--- API/AplicacionIngSoftFinal/src/main/java/com/dirac/aplicacioningsoftfinal/Controller/CategoriaController.java
+++ API/AplicacionIngSoftFinal/src/main/java/com/dirac/aplicacioningsoftfinal/Controller/CategoriaController.java
@@ -2,6 +2,7 @@ package com.dirac.aplicacioningsoftfinal.Controller;
 
 import com.dirac.aplicacioningsoftfinal.DTO.Res;
 import com.dirac.aplicacioningsoftfinal.Exception.IdNotFoundException;
+import com.dirac.aplicacioningsoftfinal.Model.CategoriaModel;
 import com.dirac.aplicacioningsoftfinal.Service.ICategoriaService;
 
 import org.apache.catalina.connector.Response;
@@ -11,6 +12,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -82,4 +86,36 @@ public class CategoriaController {
         return ResponseEntity.status(res.getStatus()).body(res);
 
     }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createCategory(@RequestBody CategoriaModel categoria) {
+        try {
+            CategoriaModel createdCategory = categoriaService.createCategory(categoria);
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdCategory);
+        } catch (Exception e) {
+            Res response = new Res();
+            response.setMessage("Error al crear la categoría: " + e.getMessage());
+            response.setStatus(500);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateCategory(@PathVariable("id") String id, @RequestBody CategoriaModel categoria) {
+        try {
+            CategoriaModel updatedCategory = categoriaService.updateCategory(id, categoria);
+            return ResponseEntity.status(HttpStatus.OK).body(updatedCategory);
+        } catch (IdNotFoundException e) {
+            Res response = new Res();
+            response.setMessage("No se encontró la categoría con ID: " + id);
+            response.setStatus(404);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        } catch (Exception e) {
+            Res response = new Res();
+            response.setMessage("Error al actualizar la categoría: " + e.getMessage());
+            response.setStatus(500);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
+    }
+
 }
